26e5b9168fc43dc7b18fdcfc9f6a1910389f8f2f
Tu Vu <tuvu23061992@gmail.com>
2016-09-18 22:03:47 -0500

    TimeCenter helper 1st commit

---
+++ TimeCenterHelper/TimeCenterHelperService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCenterHelper
+{
+    public partial class TimeCenterHelperService : ServiceBase
+    {
+        private readonly string sourceName = "TimeCenterHelper";
+        private readonly string logName = "TimeCenterHelperLog";
+
+        public TimeCenterHelperService()
+        {
+            InitializeComponent();
+            if (!System.Diagnostics.EventLog.SourceExists(sourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(sourceName, logName);
+            }
+            TimeCenterEventLog.Source = sourceName;
+            TimeCenterEventLog.Log = logName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            TimeCenterEventLog.WriteEntry("In OnStart");
+            forms
+        }
+
+        protected override void OnContinue()
+        {
+            TimeCenterEventLog.WriteEntry("In OnContinue.");
+        }
+
+        protected override void OnStop()
+        {
+            TimeCenterEventLog.WriteEntry("In OnStop");
+        }
+    }
+}
